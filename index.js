fa89eb6850eeaf94184f4612f0fff127f37f0091
Timothy Chang <123012347+Proton2050@users.noreply.github.com>
2023-05-25 09:34:04 -0700

    Add Book functional

--- index.js
+++ index.js
@@ -3,17 +3,78 @@ const bookshelfElement = document.querySelector(".books");
 const bookshelf = new Bookshelf(bookshelfElement);
 bookshelf.seed(bookData);
 
-// Search by title.
+// -------------------------------Search & Reset----------------------------------------------
+// Search by title or author
 const searchInput = document.querySelector(".search-input");
 const searchBtn = document.querySelector(".search-button");
 
+// Enable search button to initiate search
 searchBtn.addEventListener("click", () => {
     const query = searchInput.value.toLowerCase();
     const searchFn = (book) => book.title.toLowerCase().includes(query) || book.author.some((author) =>
     author.toLowerCase().includes(query));
     bookshelf.filterVisibleBooks(searchFn);
 });
+// Enable enter key to initiate search
+searchInput.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    const query = searchInput.value.toLowerCase();
+    const searchFn = (book) => book.title.toLowerCase().includes(query) || book.author.some((author) =>
+    author.toLowerCase().includes(query));
+    bookshelf.filterVisibleBooks(searchFn);
+  }
+});
 
-// Sort
+// Reset search filter
+const resetBtn = document.querySelector(".reset");
+
+resetBtn.addEventListener("click", () => {
+    const query = "";
+    const searchFn = (book) => book.title.toLowerCase().includes(query) || book.author.some((author) =>
+    author.toLowerCase().includes(query));
+    bookshelf.filterVisibleBooks(searchFn);
+    searchInput.value = "";
+})
+
+// -------------------------------Sort-----------------------------------------------
+// Sort by title
 const sortBy = document.querySelector(".sort-select");
+sortBy.addEventListener("change", () => {
+    const query = sortBy.value;
+    let sortFn;
 
+    if (query ==="title-alphabetical") {
+        sortFn = (a, z) => a.title.localeCompare(z.title);
+    } else if (query === "title-reverse-alphabetical") {
+        sortFn = (a, z) => z.title.localeCompare(a.title);
+    }
+
+    bookshelf.sortVisibleBooks(sortFn);
+});
+
+// ----------------------------Add Book----------------------------------------------
+const titleInput = document.querySelector(".title-input");
+const authorInput = document.querySelector(".author-input");
+const subjectInput = document.querySelector(".subject-input");
+const langInput = document.querySelector(".language-input");
+const addBookBtn = document.querySelector(".add-book-button");
+
+addBookBtn.addEventListener("click", () => {
+    const title = titleInput.value;
+    const author = authorInput.value;
+    const subject = subjectInput.value;
+    const language = langInput.value;
+    let newBook = new Book(title, [author], [subject], language);
+
+    // Require title and author inputs
+    if (title === "" || author === "") {
+        alert("Please fill in the title and author fields.");
+        return;
+    }
+    bookshelf.addBook(newBook);
+    bookshelf.render();
+
+    // Clear inputs
+    let allInputs = document.querySelectorAll("input");
+    allInputs.forEach((input) => input.value = "");
+})
--- js/book.js
+++ js/book.js
@@ -1,10 +1,10 @@
 class Book {
     constructor(title, author, subject, language) {
-        this.title = title;
-        this.author = author;
-        this.subject = subject;
-        this.language = language;
-        this.isFavorite = false;
+        this.title = title; //string
+        this.author = author; //array
+        this.subject = subject; //array
+        this.language = language; //string
+        this.isFavorite = false; //boolean
     }
 
     // Return List Item DOM element representing the book
